642dc279dba871013a4b27038f59a705b94b209d
peeyush1234 <peeyush.hsuyeep@gmail.com>
2017-03-26 16:18:35 -0700

    Let's encryt setup

--- src/server.js
+++ src/server.js
@@ -104,6 +104,12 @@ app.use('/graphql', expressGraphQL(req => ({
   pretty: __DEV__,
 })));
 
+// Let's encrypt setup
+// ----------------------
+app.get('/.well-known/acme-challenge/:name', (req, res) => {
+  res.sendFile(req.params.name, { root: './public' });
+});
+
 //
 // Register server-side rendering middleware
 // -----------------------------------------------------------------------------
